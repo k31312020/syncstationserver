c389ad78f05763bdf83b9a2a495581ff628db95a
Kuenzang Gyeltshen <kuenzngg@gmail.com>
2024-05-13 16:58:46 +1000

    add users model

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,30 @@
+require 'swagger_helper'
+
+RSpec.describe 'users', type: :request do
+
+  path '/users' do
+
+    post('create user') do
+      consumes 'application/json'
+      parameter name: :params, in: :body, schema: {
+        type: :object,
+        properties: {
+          email: { type: :string },
+          password: { type: :string }
+        },
+        required: %w[email password]
+      }
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end
